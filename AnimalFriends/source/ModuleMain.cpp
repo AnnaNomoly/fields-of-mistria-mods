49e9dabefe7f04ba0c2d2b8861a0ba98b57fa0a4
AnnaNomoly <annanomoly@outlook.com>
2025-02-05 17:54:31 -0500

    AnimalFriends
    Removed functions that silenced the game's log output.

--- AnimalFriends/source/ModuleMain.cpp
+++ AnimalFriends/source/ModuleMain.cpp
@@ -449,50 +449,6 @@ RValue& GmlScriptOnNewDayCallback(
 	return Result;
 }
 
-RValue& GmlScriptTraceCallback(
-	IN CInstance* Self,
-	IN CInstance* Other,
-	OUT RValue& Result,
-	IN int ArgumentCount,
-	IN RValue** Arguments
-)
-{
-	return Result;
-}
-
-RValue& GmlScriptInfoCallback(
-	IN CInstance* Self,
-	IN CInstance* Other,
-	OUT RValue& Result,
-	IN int ArgumentCount,
-	IN RValue** Arguments
-)
-{
-	return Result;
-}
-
-RValue& GmlScriptWarnCallback(
-	IN CInstance* Self,
-	IN CInstance* Other,
-	OUT RValue& Result,
-	IN int ArgumentCount,
-	IN RValue** Arguments
-)
-{
-	return Result;
-}
-
-RValue& GmlScriptErrorCallback(
-	IN CInstance* Self,
-	IN CInstance* Other,
-	OUT RValue& Result,
-	IN int ArgumentCount,
-	IN RValue** Arguments
-)
-{
-	return Result;
-}
-
 void CreateHookEventObject(AurieStatus& status)
 {
 	status = g_ModuleInterface->CreateCallback(
@@ -589,114 +545,6 @@ void CreateHookGmlScriptOnNewDay(AurieStatus& status)
 	}
 }
 
-void CreateHookGmlScriptTrace(AurieStatus& status)
-{
-	CScript* gml_script_trace = nullptr;
-	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_trace",
-		(PVOID*)&gml_script_trace
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to get script (gml_Script_trace)!", VERSION);
-	}
-
-	status = MmCreateHook(
-		g_ArSelfModule,
-		"gml_Script_trace",
-		gml_script_trace->m_Functions->m_ScriptFunction,
-		GmlScriptTraceCallback,
-		nullptr
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to hook script (gml_Script_trace)!", VERSION);
-	}
-}
-
-void CreateHookGmlScriptInfo(AurieStatus& status)
-{
-	CScript* gml_script_info = nullptr;
-	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_info",
-		(PVOID*)&gml_script_info
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to get script (gml_Script_info)!", VERSION);
-	}
-
-	status = MmCreateHook(
-		g_ArSelfModule,
-		"gml_Script_info",
-		gml_script_info->m_Functions->m_ScriptFunction,
-		GmlScriptInfoCallback,
-		nullptr
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to hook script (gml_Script_info)!", VERSION);
-	}
-}
-
-void CreateHookGmlScriptWarn(AurieStatus& status)
-{
-	CScript* gml_script_warn = nullptr;
-	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_warn",
-		(PVOID*)&gml_script_warn
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to get script (gml_Script_warn)!", VERSION);
-	}
-
-	status = MmCreateHook(
-		g_ArSelfModule,
-		"gml_Script_warn",
-		gml_script_warn->m_Functions->m_ScriptFunction,
-		GmlScriptWarnCallback,
-		nullptr
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to hook script (gml_Script_warn)!", VERSION);
-	}
-}
-
-void CreateHookGmlScriptError(AurieStatus& status)
-{
-	CScript* gml_script_error = nullptr;
-	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_error",
-		(PVOID*)&gml_script_error
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to get script (gml_Script_error)!", VERSION);
-	}
-
-	status = MmCreateHook(
-		g_ArSelfModule,
-		"gml_Script_error",
-		gml_script_error->m_Functions->m_ScriptFunction,
-		GmlScriptErrorCallback,
-		nullptr
-	);
-
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Failed to hook script (gml_Script_error)!", VERSION);
-	}
-}
-
 EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path& ModulePath) {
 	UNREFERENCED_PARAMETER(ModulePath);
 
@@ -734,34 +582,6 @@ EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path&
 	}
 
 	CreateHookGmlScriptOnNewDay(status);
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Exiting due to failure on start!", VERSION);
-		return status;
-	}
-
-	CreateHookGmlScriptTrace(status);
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Exiting due to failure on start!", VERSION);
-		return status;
-	}
-
-	CreateHookGmlScriptInfo(status);
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Exiting due to failure on start!", VERSION);
-		return status;
-	}
-
-	CreateHookGmlScriptWarn(status);
-	if (!AurieSuccess(status))
-	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Exiting due to failure on start!", VERSION);
-		return status;
-	}
-
-	CreateHookGmlScriptError(status);
 	if (!AurieSuccess(status))
 	{
 		g_ModuleInterface->Print(CM_LIGHTRED, "[AnimalFriends %s] - Exiting due to failure on start!", VERSION);
