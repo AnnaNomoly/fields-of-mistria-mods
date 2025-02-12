93c1d028324d5b5041db1c0dca41bd10b7cdd5a4
AnnaNomoly <annanomoly@outlook.com>
2025-02-12 14:34:27 -0500

    DontStarve
    Item and recipe data is now loaded directly from the game and not hard coded.

--- DontStarve/source/ModuleMain.cpp
+++ DontStarve/source/ModuleMain.cpp
@@ -11,224 +11,19 @@ static const int HEALTH_LOST_PER_TICK = -10;
 static const int STARTING_HUNGER_VALUE = 100;
 
 static YYTKInterface* g_ModuleInterface = nullptr;
+static bool load_items = true;
 static int ari_hunger_value = STARTING_HUNGER_VALUE;
 static bool ari_is_hungry = false;
 static bool is_tracked_time_interval = false;
 static int time_of_last_tick = 0; // TODO: Set this on file load.
 static int held_item_id = -1;
-static std::string held_item_name;
 static bool game_is_active = false;
 static bool health_bar_visible = false;
 static bool use_health_instead_of_stamina = false;
 static int hunger_stamina_health_penatly = 0;
 
-static int font = 0; // DEBUG
-
-static std::map<std::string, int> item_name_to_stars_map = {
-	// Forage
-	{ "apple", 5 },
-	{ "basil", 5 },
-	{ "blackberry", 1 },
-	{ "blueberry", 1 },
-	{ "burdock_root", 8 },
-	{ "cherry", 5 },
-	{ "chestnut", 3 },
-	{ "chickpea", 3 },
-	{ "coconut", 6 },
-	{ "dill", 5 },
-	{ "fennel", 3 },
-	{ "fiddlehead", 5 },
-	{ "garlic", 6 },
-	{ "glowberry", 1 },
-	{ "horseradish", 3 },
-	{ "ice_block", 6 },
-	{ "jasmine", 3 },
-	{ "lemon", 5 },
-	{ "moon_fruit", 8 },
-	{ "morel_mushroom", 5 },
-	{ "nettle", 5 },
-	{ "orange", 5 },
-	{ "oregano", 5 },
-	{ "oyster_mushroom", 5 },
-	{ "peach", 5 },
-	{ "pear", 5 },
-	{ "pineshroom", 5 },
-	{ "pomegranate", 5 },
-	{ "rose", 3 },
-	{ "rose_hip", 5 },
-	{ "rosemary", 6 },
-	{ "sage", 5 },
-	{ "sesame", 5 },
-	{ "thyme", 5 },
-	{ "underseaweed", 5 },
-	{ "water_chestnut", 3 },
-	{ "wild_berries", 1 },
-	{ "wild_grapes", 6 },
-	{ "wild_leek", 3 },
-	{ "wintergreen_berry", 3 },
-
-	// Cooked Dishes
-	{ "apple_honey_curry", 50 },
-	{ "apple_juice", 10 },
-	{ "apple_pie", 20 },
-	{ "baked_potato", 10 },
-	{ "baked_sweetroot", 10 },
-	{ "beet_salad", 10 },
-	{ "beet_soup", 50 },
-	{ "berries_and_cream", 10 },
-	{ "berry_bowl", 30 },
-	{ "blackberry_jam", 10 },
-	{ "blueberry_jam", 10 },
-	{ "braised_burdock", 20 },
-	{ "braised_carrots", 10 },
-	{ "bread", 10 },
-	{ "breaded_catfish", 20 },
-	{ "broccoli_salad", 20 },
-	{ "buttered_peas", 10 },
-	{ "cabbage_slaw", 10 },
-	{ "candied_lemon_peel", 10 },
-	{ "candied_strawberries", 10 },
-	{ "canned_sardines", 10 },
-	{ "caramel_candy", 10 },
-	{ "caramelized_moon_fruit", 10 },
-	{ "cauliflower_curry", 30 },
-	{ "cherry_cobbler", 10 },
-	{ "cherry_smoothie", 20 },
-	{ "cherry_tart", 20 },
-	{ "chickpea_curry", 20 },
-	{ "chili_coconut_curry", 40 },
-	{ "chocolate_cake", 30 },
-	{ "clam_chowder", 20 },
-	{ "coconut_cream_pie", 40 },
-	{ "coconut_milk", 10 },
-	{ "cod_with_thyme", 20 },
-	{ "crab_cakes", 40 },
-	{ "cranberry_juice", 10 },
-	{ "cranberry_orange_scone", 40 },
-	{ "crayfish_etouffee", 20 },
-	{ "crispy_fried_earthshroom", 20 },
-	{ "crunchy_chickpeas", 10 },
-	{ "crystal_berry_pie", 30 },
-	{ "cucumber_salad", 20 },
-	{ "cucumber_sandwich", 30 },
-	{ "deep_sea_soup", 20 },
-	{ "deluxe_curry", 30 },
-	{ "deviled_eggs", 20 },
-	{ "dried_squid", 10 },
-	{ "fish_skewers", 10 },
-	{ "fish_stew", 20 },
-	{ "fish_tacos", 40 },
-	{ "fried_rice", 50 },
-	{ "garlic_bread", 10 },
-	{ "gazpacho", 30 },
-	{ "glowberry_cookies", 40 },
-	{ "golden_cheesecake", 50 },
-	{ "golden_cookies", 50 },
-	{ "grape_juice", 10 },
-	{ "green_tea", 10 },
-	{ "grilled_cheese", 30 },
-	{ "grilled_corn", 10 },
-	{ "grilled_eel_rice_bowl", 30 },
-	{ "hard_boiled_egg", 10 },
-	{ "harvest_plate", 50 },
-	{ "herb_butter_pasta", 30 },
-	{ "herb_salad", 40 },
-	{ "horseradish_salmon", 30 },
-	{ "hot_chocolate", 30 },
-	{ "ice_cream_sundae", 50 },
-	{ "iced_coffee", 20 },
-	{ "incredibly_hot_pot", 40 },
-	{ "jam_sandwich", 20 },
-	{ "jasmine_tea", 10 },
-	{ "latte", 10 },
-	{ "lemon_cake", 20 },
-	{ "lemon_pie", 30 },
-	{ "lemonade", 10 },
-	{ "loaded_baked_potato", 30 },
-	{ "lobster_roll", 40 },
-	{ "mackerel_sashimi", 10 },
-	{ "marmalade", 10 },
-	{ "miner's_mushroom_stew", 10 },
-	{ "mocha", 20 },
-	{ "monster_cookies", 20 },
-	{ "monster_mash", 30 },
-	{ "mont_blanc", 50 },
-	{ "moon_fruit_cake", 30 },
-	{ "mushroom_rice", 20 },
-	{ "mushroom_steak_dinner", 40 },
-	{ "noodles", 10 },
-	{ "omelet", 30 },
-	{ "onion_soup", 20 },
-	{ "orange_juice", 10 },
-	{ "pan_fried_bream", 10 },
-	{ "pan-fried_salmon", 10 },
-	{ "pan-fried_snapper", 10 },
-	{ "peaches_and_cream", 20 },
-	{ "perch_risotto", 40 },
-	{ "pineshroom_toast", 20 },
-	{ "pizza", 20 },
-	{ "poached_pear", 20 },
-	{ "pomegranate_juice", 10 },
-	{ "pomegranate_sorbet", 20 },
-	{ "potato_soup", 20 },
-	{ "pudding", 20 },
-	{ "pumpkin_pie", 40 },
-	{ "pumpkin_stew", 30 },
-	{ "quiche", 20 },
-	{ "red_snapper_sushi", 10 },
-	{ "rice_ball", 20 },
-	{ "roasted_cauliflower", 10 },
-	{ "roasted_chestnuts", 10 },
-	{ "roasted_rice_tea", 10 },
-	{ "roasted_sweet_potato", 10 },
-	{ "rose_hip_jam", 10 },
-	{ "rose_tea", 10 },
-	{ "rosemary_garlic_noodles", 30 },
-	{ "salmon_sashimi", 10 },
-	{ "salted_watermelon", 10 },
-	{ "sauteed_snow_peas", 10 },
-	{ "sea_bream_rice", 40 },
-	{ "seafood_boil", 50 },
-	{ "seafood_snow_pea_noodles", 50 },
-	{ "seaweed_salad", 20 },
-	{ "sesame_broccoli", 10 },
-	{ "sesame_tuna_bowl", 30 },
-	{ "simmered_daikon", 10 },
-	{ "sliced_turnip", 10 },
-	{ "smoked_trout_soup", 10 },
-	{ "spell_fruit_parfait", 50 },
-	{ "spicy_cheddar_biscuit", 30 },
-	{ "spicy_corn", 20 },
-	{ "spicy_crab_sushi", 30 },
-	{ "spicy_water_chestnuts", 10 },
-	{ "spring_galette", 50 },
-	{ "spring_salad", 20 },
-	{ "steamed_broccoli", 10 },
-	{ "strawberries_and_cream", 10 },
-	{ "strawberry_shortcake", 30 },
-	{ "summer_salad", 30 },
-	{ "sushi_platter", 50 },
-	{ "sweet_potato_pie", 20 },
-	{ "sweet_sesame_balls", 30 },
-	{ "tea_with_lemon", 20 },
-	{ "tide_salad", 30 },
-	{ "toasted_sunflower_seeds", 20 },
-	{ "tomato_soup", 20 },
-	{ "trail_mix", 10 },
-	{ "tuna_sashimi", 10 },
-	{ "turnip_&_cabbage_salad", 20 },
-	{ "turnip_&_potato_gratin", 30 },
-	{ "vegetable_pot_pie", 40 },
-	{ "vegetable_quiche", 40 },
-	{ "vegetable_soup", 20 },
-	{ "veggie_sub_sandwich", 50 },
-	{ "water_chestnut_fritters", 20 },
-	{ "wild_berry_jam", 10 },
-	{ "wild_berry_pie", 20 },
-	{ "wild_berry_scone", 20 },
-	{ "winter_stew", 30 },
-	{ "wintergreen_ice_cream", 20 },
-};
+static std::map<std::string, int> item_name_to_restoration_map = {}; // Excludes cooked dishes
+static std::map<std::string, int> recipe_name_to_stars_map = {}; // Only cooked dishes
 
 bool GameIsPaused()
 {
@@ -337,9 +132,10 @@ void ObjectCallback(
 	);
 
 	// Flag when the health bar is visible.
-	if (ari_current_health.m_Real < ari_max_health.m_Real) {
+	if (ari_current_health.m_Real < ari_max_health.m_Real)
 		health_bar_visible = true;
-	}
+	else
+		health_bar_visible = false;
 }
 
 RValue& GmlScriptGetMinutesCallback(
@@ -399,45 +195,49 @@ RValue& GmlScriptModifyStaminaCallback(
 			(PVOID*)&gml_script_item_id_to_name
 		);
 
-		RValue item_id_as_string;
-		RValue* item_id = new RValue(held_item_id);
+		RValue item_name;
+		RValue item_id = held_item_id;
+		RValue* item_id_ptr = &item_id;
 		gml_script_item_id_to_name->m_Functions->m_ScriptFunction(
 			Self,
 			Other,
-			item_id_as_string,
+			item_name,
 			1,
-			{ &item_id }
+			{ &item_id_ptr }
 		);
 
-		if (item_name_to_stars_map.count(item_id_as_string.AsString().data()) > 0)
+		//if (item_name_to_stars_map.count(item_id_as_string.AsString().data()) > 0)
+		int modifier = 0;
+		if (recipe_name_to_stars_map.count(item_name.AsString().data()) > 0)
 		{
-			ari_hunger_value += 2 * item_name_to_stars_map[item_id_as_string.AsString().data()]; // Testing 2x multiplier for balancing.
-			if (ari_hunger_value > 100) {
-				ari_hunger_value = 100;
-			}
-			g_ModuleInterface->Print(CM_LIGHTGREEN, "[DontStarve] - Increased hunger meter by %d!", item_name_to_stars_map[item_id_as_string.AsString().data()]);
+			modifier = 2 * (recipe_name_to_stars_map[item_name.AsString().data()] * 10); // Testing 2x multiplier for balancing.
 		}
-		else {
-			ari_hunger_value += 2 * Arguments[0]->m_Real; // Testing 2x multiplier for balancing.
-
-			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[DontStarve] - Item not in hunger lookup dictionary: (%d) %s", item_id, item_id_as_string.AsString().data());
-			g_ModuleInterface->Print(CM_LIGHTGREEN, "[DontStarve] - Increased hunger meter by %d!", static_cast<int>(Arguments[0]->m_Real));
+		else if (item_name_to_restoration_map.count(item_name.AsString().data()) > 0)
+		{
+			modifier = item_name_to_restoration_map[item_name.AsString().data()];
+		}
+		else
+		{
+			modifier = 2 * Arguments[0]->m_Real; // Testing 2x multiplier for balancing.
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[DontStarve] - Item not in lookup dictionaries: (%d) %s", held_item_id, item_name.AsString().data());
 		}
 
-		delete item_id;
+		ari_hunger_value += modifier;
+		if (ari_hunger_value > 100) {
+			ari_hunger_value = 100;
+		}
+		g_ModuleInterface->Print(CM_LIGHTGREEN, "[DontStarve] - Increased hunger meter by %d!", modifier);
 	}
 	else {
-		int temp = ari_hunger_value += Arguments[0]->m_Real;
-		if (temp < 0) {
+		int new_hunger_value = ari_hunger_value + Arguments[0]->m_Real;
+		if (new_hunger_value < 0) {
 			use_health_instead_of_stamina = true;
-			hunger_stamina_health_penatly += temp;
+			hunger_stamina_health_penatly += new_hunger_value;
 			ari_hunger_value = 0;
+			g_ModuleInterface->Print(CM_LIGHTGREEN, "[DontStarve] - Ari is hungry! Using health in place of stamina.");
 		}
 		else {
-			ari_hunger_value += Arguments[0]->m_Real;
-			if (ari_hunger_value < 0) {
-				ari_hunger_value = 0;
-			}
+			ari_hunger_value = new_hunger_value;
 			g_ModuleInterface->Print(CM_LIGHTGREEN, "[DontStarve] - Decreased hunger meter by %d!", static_cast<int>(Arguments[0]->m_Real));
 		}
 	}
@@ -469,6 +269,8 @@ RValue& GmlScriptOnDrawGuiCallback(
 		ArgumentCount,
 		Arguments
 	);
+
+	// TODO: Look into using Archie's code to detect a sprite's X,Y position and offset to figure out position and size for hunger meter and icon.
 
 	// Don't let hunger meter fall below 0.
 	if (ari_hunger_value < 0)
@@ -608,9 +410,68 @@ RValue& GmlScriptSetupMainScreenCallback(
 	IN RValue** Arguments
 )
 {
-	game_is_active = false;
-	time_of_last_tick = 0;
-	ari_hunger_value = STARTING_HUNGER_VALUE;
+	if (load_items)
+	{
+		CInstance* global_instance = nullptr;
+		g_ModuleInterface->GetGlobalInstance(&global_instance);
+
+		RValue __item_data = global_instance->at("__item_data");
+
+		size_t array_length;
+		g_ModuleInterface->GetArraySize(__item_data, array_length);
+
+		for (size_t i = 0; i < array_length; i++)
+		{
+			RValue* array_element;
+			g_ModuleInterface->GetArrayEntry(__item_data, i, array_element);
+
+			RValue name_key = array_element->at("name_key");
+			if (name_key.m_Kind != VALUE_NULL && name_key.m_Kind != VALUE_UNDEFINED && name_key.m_Kind != VALUE_UNSET)
+			{
+				RValue recipe_key = array_element->at("recipe_key");
+
+				if (strstr(name_key.AsString().data(), "cooked_dishes"))
+				{
+					RValue recipe = array_element->at("recipe");
+					if (recipe.m_Kind != VALUE_NULL && recipe.m_Kind != VALUE_UNDEFINED && recipe.m_Kind != VALUE_UNSET)
+					{
+						//RValue item_id = recipe.at("item_id");
+						if (recipe_name_to_stars_map.count(recipe_key.AsString().data()) <= 0)
+						{
+							RValue stars = array_element->at("stars");
+							if (stars.m_Kind != VALUE_NULL && stars.m_Kind != VALUE_UNDEFINED && stars.m_Kind != VALUE_UNSET)
+								recipe_name_to_stars_map[recipe_key.AsString().data()] = stars.m_Real;
+							else
+							{
+								g_ModuleInterface->Print(CM_LIGHTYELLOW, "[DontStarve %s] - Missing \"stars\" data for recipe: %s", recipe_key.AsString().data());
+								recipe_name_to_stars_map[recipe_key.AsString().data()] = 1;
+							}
+						}
+					}
+				}
+				else
+				{
+					RValue edible = array_element->at("edible");
+					if (edible.m_Kind == VALUE_BOOL && edible.m_Real == 1.0)
+					{
+						if (item_name_to_restoration_map.count(recipe_key.AsString().data()) <= 0)
+						{
+							RValue stamina_modifier = array_element->at("stamina_modifier");
+							item_name_to_restoration_map[recipe_key.AsString().data()] = stamina_modifier.m_Real;
+						}
+					}
+				}
+			}
+		}
+
+		load_items = false;
+	}
+	else
+	{
+		game_is_active = false;
+		time_of_last_tick = 0;
+		ari_hunger_value = STARTING_HUNGER_VALUE;
+	}
 
 	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_setup_main_screen@TitleMenu@TitleMenu"));
 	original(
