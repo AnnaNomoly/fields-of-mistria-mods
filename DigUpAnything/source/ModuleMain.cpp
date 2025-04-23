d4d3d5b2bdc751540ef3ef31b5587c52113f68fd
AnnaNomoly <annanomoly@outlook.com>
2025-04-23 13:58:17 -0400

    DigUpAnything
    1.0.3 release. Information regarding the input value will be displayed via in-game notification in addition to the YYTK log. Updated disabled item list. Added mod interoperability for SecretSanta and Curator.

--- DigUpAnything/source/ModuleMain.cpp
+++ DigUpAnything/source/ModuleMain.cpp
@@ -5,13 +5,123 @@ using namespace YYTK;
 
 static YYTKInterface* g_ModuleInterface = nullptr;
 
-static const char* const VERSION = "1.0.2";
-static const std::string DISABLED_ITEMS[] = { "animal_cosmetic", "cosmetic", "crafting_scroll", "recipe_scroll", "unidentified_artifact" };
+static const char* const MOD_NAME = "DigUpAnything";
+static const char* const VERSION = "1.0.3";
+static const std::string VALID_ITEM_LOCALIZATION_KEY = "mods/DigUpAnything/valid_item";
+static const std::string DISABLED_ITEM_LOCALIZATION_KEY = "mods/DigUpAnything/disabled_item";
+static const std::string UNRECOGNIZED_ITEM_LOCALIZATION_KEY = "mods/DigUpAnything/unrecognized_item";
+static const std::string DISABLED_ITEMS[] = { "animal_cosmetic", "cosmetic", "crafting_scroll", "purse", "recipe_scroll", "unidentified_artifact" };
 
+static RValue __YYTK;
 static int item_id;
-static bool load_items = true;
+static bool load_on_start = true;
 static bool override_next_dig_spot = false;
 static std::map<std::string, int> item_name_to_id_map = {};
+static std::string internal_item_name = "";
+
+bool RValueAsBool(RValue value)
+{
+	if (value.m_Kind == VALUE_BOOL && value.m_Real == 1)
+		return true;
+	return false;
+}
+
+bool GlobalVariableExists(const char* variable_name)
+{
+	RValue global_variable_exists = g_ModuleInterface->CallBuiltin(
+		"variable_global_exists",
+		{ variable_name }
+	);
+
+	return RValueAsBool(global_variable_exists);
+}
+
+RValue GlobalVariableGet(const char* variable_name)
+{
+	return g_ModuleInterface->CallBuiltin(
+		"variable_global_get",
+		{ variable_name }
+	);
+}
+
+RValue GlobalVariableSet(const char* variable_name, RValue value)
+{
+	return g_ModuleInterface->CallBuiltin(
+		"variable_global_set",
+		{ variable_name, value }
+	);
+}
+
+bool StructVariableExists(RValue the_struct, const char* variable_name)
+{
+	RValue struct_exists = g_ModuleInterface->CallBuiltin(
+		"struct_exists",
+		{ the_struct, variable_name }
+	);
+
+	return RValueAsBool(struct_exists);
+}
+
+RValue StructVariableGet(RValue the_struct, const char* variable_name)
+{
+	return g_ModuleInterface->CallBuiltin(
+		"struct_get",
+		{ the_struct, variable_name }
+	);
+}
+
+RValue StructVariableSet(RValue the_struct, const char* variable_name, RValue value)
+{
+	return g_ModuleInterface->CallBuiltin(
+		"struct_set",
+		{ the_struct, variable_name, value }
+	);
+}
+
+void CreateOrGetGlobalYYTKVariable()
+{
+	if (!GlobalVariableExists("__YYTK"))
+	{
+		g_ModuleInterface->GetRunnerInterface().StructCreate(&__YYTK);
+		GlobalVariableSet("__YYTK", __YYTK);
+	}
+	else
+		__YYTK = GlobalVariableGet("__YYTK");
+}
+
+void CreateModInfoInGlobalYYTKVariable()
+{
+	if (!StructVariableExists(__YYTK, MOD_NAME))
+	{
+		RValue DigUpAnything;
+		RValue version = VERSION;
+		g_ModuleInterface->GetRunnerInterface().StructCreate(&DigUpAnything);
+		g_ModuleInterface->GetRunnerInterface().StructAddRValue(&DigUpAnything, "version", &version);
+		g_ModuleInterface->GetRunnerInterface().StructAddRValue(&__YYTK, MOD_NAME, &DigUpAnything);
+	}
+}
+
+void DisplayNotification(CInstance* Self, CInstance* Other, std::string localization_key)
+{
+	CScript* gml_script_create_notification = nullptr;
+	g_ModuleInterface->GetNamedRoutinePointer(
+		"gml_Script_create_notification",
+		(PVOID*)&gml_script_create_notification
+	);
+
+	RValue result;
+	RValue notification = localization_key;
+	RValue* notification_ptr = &notification;
+	gml_script_create_notification->m_Functions->m_ScriptFunction(
+		Self,
+		Other,
+		result,
+		1,
+		{ &notification_ptr }
+	);
+
+	internal_item_name = "";
+}
 
 RValue& GmlScriptChooseRandomArtifactCallback(
 	IN CInstance* Self,
@@ -64,15 +174,32 @@ RValue& GmlScriptHeldItemCallback(
 		{
 			auto disabled_item = std::find(std::begin(DISABLED_ITEMS), std::end(DISABLED_ITEMS), user_input_string.AsString().data());
 			if (disabled_item != std::end(DISABLED_ITEMS)) {
+				DisplayNotification(Self, Other, DISABLED_ITEM_LOCALIZATION_KEY);
 				g_ModuleInterface->Print(CM_LIGHTYELLOW, "[DigUpAnything %s] - Ignoring disabled item: %s", VERSION, user_input_string.AsString().data());
 			}
 			else {
+				// Indicate to other mods the dig site is being modified by DigUpAnything.
+				if (StructVariableExists(__YYTK, "SecretSanta"))
+				{
+					RValue SecretSanta = StructVariableGet(__YYTK, "SecretSanta");
+					RValue ignore_next_dig_spot = true;
+					StructVariableSet(SecretSanta, "ignore_next_dig_spot", ignore_next_dig_spot);
+				}
+				if (StructVariableExists(__YYTK, "Curator"))
+				{
+					RValue Curator = StructVariableGet(__YYTK, "Curator");
+					RValue ignore_next_dig_spot = true;
+					StructVariableSet(Curator, "ignore_next_dig_spot", ignore_next_dig_spot);
+				}
+				
 				item_id = item_name_to_id_map[user_input_string.AsString().data()];
 				override_next_dig_spot = true;
+				DisplayNotification(Self, Other, VALID_ITEM_LOCALIZATION_KEY);
 				g_ModuleInterface->Print(CM_LIGHTGREEN, "[DigUpAnything %s] - Next artifact spot will be: %s", VERSION, user_input_string.AsString().data());
 			}
 		}
 		else {
+			DisplayNotification(Self, Other, UNRECOGNIZED_ITEM_LOCALIZATION_KEY);
 			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[DigUpAnything %s] - Ignoring unrecognized item: %s", VERSION, user_input_string.AsString().data());
 		}
 
@@ -98,9 +225,12 @@ RValue& GmlScriptSetupMainScreenCallback(
 	IN RValue** Arguments
 )
 {
-	if (load_items)
+	if (load_on_start)
 	{
-		for (int64_t i = 0; i < 2000; i++)
+		CreateOrGetGlobalYYTKVariable();
+		CreateModInfoInGlobalYYTKVariable();
+
+		for (int64_t i = 0; i < 3000; i++)
 		{
 			CScript* gml_script_try_item_id_to_string = nullptr;
 			g_ModuleInterface->GetNamedRoutinePointer(
@@ -136,7 +266,7 @@ RValue& GmlScriptSetupMainScreenCallback(
 			g_ModuleInterface->Print(CM_LIGHTRED, "[DigUpAnything %s] - Failed to load data for items!", VERSION);
 		}
 
-		load_items = false;
+		load_on_start = false;
 	}
 
 	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_setup_main_screen@TitleMenu@TitleMenu"));
@@ -248,7 +378,7 @@ EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path&
 	if (!AurieSuccess(status))
 		return AURIE_MODULE_DEPENDENCY_NOT_RESOLVED;
 
-	g_ModuleInterface->Print(CM_LIGHTGREEN, "[DigUpAnything %s] - Plugin starting...", VERSION);
+	g_ModuleInterface->Print(CM_LIGHTAQUA, "[DigUpAnything %s] - Plugin starting...", VERSION);
 
 	CreateHookGmlScriptChooseRandomArtifact(status);
 	if (!AurieSuccess(status))
