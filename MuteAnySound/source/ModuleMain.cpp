2c5a080853ba7bb866f24f0ed646c8f511a97eef
AnnaNomoly <annanomoly@outlook.com>
2025-11-01 23:47:15 -0400

    MuteAnySound
    Initial commit.

---
+++ MuteAnySound/source/ModuleMain.cpp
@@ -0,0 +1,368 @@
+#include <map>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <YYToolkit/YYTK_Shared.hpp> // YYTK v4
+using namespace Aurie;
+using namespace YYTK;
+using json = nlohmann::json;
+
+static const char* const MOD_NAME = "MuteAnySound";
+static const char* const VERSION = "1.0.0";
+static const char* const GML_SCRIPT_SETUP_MAIN_SCREEN = "gml_Script_setup_main_screen@TitleMenu@TitleMenu";
+static const char* const GML_SCRIPT_ON_DRAW_GUI = "gml_Script_on_draw_gui@Display@Display";
+static const char* const MUTED_SOUNDS_KEY = "muted_sounds";
+
+static YYTKInterface* g_ModuleInterface = nullptr;
+static bool load_on_start = true;
+static std::vector<std::string> muted_sounds = {};
+static std::map<std::string, RValue> sound_name_to_asset_index_map = {};
+
+void handle_eptr(std::exception_ptr eptr)
+{
+	try {
+		if (eptr) {
+			std::rethrow_exception(eptr);
+		}
+	}
+	catch (const std::exception& e) {
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Error: %s", MOD_NAME, VERSION, e.what());
+	}
+}
+
+int RValueAsInt(RValue value)
+{
+	if (value.m_Kind == VALUE_REAL)
+		return static_cast<int>(value.m_Real);
+	if (value.m_Kind == VALUE_INT64)
+		return static_cast<int>(value.m_i64);
+	if (value.m_Kind == VALUE_INT32)
+		return static_cast<int>(value.m_i32);
+}
+
+bool RValueAsBool(RValue value)
+{
+	if (value.m_Kind == VALUE_REAL && value.m_Real == 1)
+		return true;
+	if (value.m_Kind == VALUE_BOOL && value.m_Real == 1)
+		return true;
+	return false;
+}
+
+bool RValueIsNumeric(RValue value)
+{
+	if (value.m_Kind == VALUE_REAL || value.m_Kind == VALUE_INT64 || value.m_Kind == VALUE_INT32)
+		return true;
+	return false;
+}
+
+bool StructVariableExists(RValue the_struct, const char* variable_name)
+{
+	RValue struct_exists = g_ModuleInterface->CallBuiltin(
+		"struct_exists",
+		{ the_struct, variable_name }
+	);
+
+	return RValueAsBool(struct_exists);
+}
+
+RValue AssetGetIndex(std::string asset_name)
+{
+	RValue asset_index = g_ModuleInterface->CallBuiltin(
+		"asset_get_index",
+		{ RValue(asset_name) }
+	);
+	return asset_index;
+}
+
+bool AudioIsPlaying(RValue sound_effect_index)
+{
+	RValue audio_is_playing = g_ModuleInterface->CallBuiltin(
+		"audio_is_playing",
+		{ sound_effect_index }
+	);
+	return RValueAsBool(audio_is_playing);
+}
+
+void StopSoundEffect(RValue sound_index)
+{
+	g_ModuleInterface->CallBuiltin(
+		"audio_stop_sound",
+		{ sound_index }
+	);
+}
+
+json CreateConfigJson(bool use_defaults)
+{
+	json config_json = {
+		{ MUTED_SOUNDS_KEY, use_defaults ? std::vector<std::string>() : muted_sounds}
+	};
+	return config_json;
+}
+
+void LogDefaultConfigValues()
+{
+	muted_sounds = {};
+
+	g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Using DEFAULT \"%s\" value: %s!", MOD_NAME, VERSION, MUTED_SOUNDS_KEY, "NONE");
+}
+
+void LoadOrCreateConfigFile()
+{
+	std::exception_ptr eptr;
+	try
+	{
+		// Try to find the mod_data directory.
+		std::string current_dir = std::filesystem::current_path().string();
+		std::string mod_data_folder = current_dir + "\\mod_data";
+		if (!std::filesystem::exists(mod_data_folder))
+		{
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"mod_data\" directory was not found. Creating directory: %s", MOD_NAME, VERSION, mod_data_folder.c_str());
+			std::filesystem::create_directory(mod_data_folder);
+		}
+
+		// Try to find the mod_data/MuteAnySound directory.
+		std::string mute_any_sound_folder = mod_data_folder + "\\MuteAnySound";
+		if (!std::filesystem::exists(mute_any_sound_folder))
+		{
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"MuteAnySound\" directory was not found. Creating directory: %s", MOD_NAME, VERSION, mute_any_sound_folder.c_str());
+			std::filesystem::create_directory(mute_any_sound_folder);
+		}
+
+		// Try to find the mod_data/MuteAnySound/MuteAnySound.json config file.
+		bool update_config_file = false;
+		std::string config_file = mute_any_sound_folder + "\\" + "MuteAnySound.json";
+		std::ifstream in_stream(config_file);
+		if (in_stream.good())
+		{
+			try
+			{
+				json json_object = json::parse(in_stream);
+
+				// Check if the json_object is empty.
+				if (json_object.empty())
+				{
+					g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - No values found in mod configuration file: %s!", MOD_NAME, VERSION, config_file.c_str());
+					g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Add your desired values to the configuration file, otherwise defaults will be used.", MOD_NAME, VERSION);
+					LogDefaultConfigValues();
+				}
+				else
+				{
+					// Try loading the muted_sounds value.
+					if (json_object.contains(MUTED_SOUNDS_KEY))
+					{
+						muted_sounds = json_object[MUTED_SOUNDS_KEY];
+						g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using CUSTOM \"%s\" value!", MOD_NAME, VERSION, MUTED_SOUNDS_KEY);
+					}
+					else
+					{
+						g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Missing \"%s\" value in mod configuration file: %s!", MOD_NAME, VERSION, MUTED_SOUNDS_KEY, config_file.c_str());
+						g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using DEFAULT \"%s\" value: %s!", MOD_NAME, VERSION, MUTED_SOUNDS_KEY, "NONE");
+					}
+				}
+
+				update_config_file = true;
+			}
+			catch (...)
+			{
+				eptr = std::current_exception();
+				handle_eptr(eptr);
+
+				g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to parse JSON from configuration file: %s", MOD_NAME, VERSION, config_file.c_str());
+				g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Make sure the file is valid JSON!", MOD_NAME, VERSION);
+				LogDefaultConfigValues();
+			}
+
+			in_stream.close();
+		}
+		else
+		{
+			in_stream.close();
+
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"MuteAnySound.json\" file was not found. Creating file: %s", MOD_NAME, VERSION, config_file.c_str());
+
+			json default_config_json = CreateConfigJson(true);
+			std::ofstream out_stream(config_file);
+			out_stream << std::setw(4) << default_config_json << std::endl;
+			out_stream.close();
+
+			LogDefaultConfigValues();
+		}
+
+		if (update_config_file)
+		{
+			json config_json = CreateConfigJson(false);
+			std::ofstream out_stream(config_file);
+			out_stream << std::setw(4) << config_json << std::endl;
+			out_stream.close();
+		}
+	}
+	catch (...)
+	{
+		eptr = std::current_exception();
+		handle_eptr(eptr);
+
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - An error occurred loading the mod configuration file.", MOD_NAME, VERSION);
+		LogDefaultConfigValues();
+	}
+}
+
+void LoadSoundAssets()
+{
+	for (std::string sound_effect_name : muted_sounds)
+	{
+		if (sound_name_to_asset_index_map.count(sound_effect_name) == 0)
+		{
+			RValue asset_index = AssetGetIndex(sound_effect_name);
+			if (asset_index.m_Kind != VALUE_UNDEFINED && asset_index.m_Kind != VALUE_UNSET && asset_index.m_Kind != VALUE_NULL)
+			{
+				sound_name_to_asset_index_map[sound_effect_name] = asset_index;
+			}
+		}
+	}
+}
+
+RValue& GmlScriptSetupMainScreenCallback(
+	IN CInstance* Self,
+	IN CInstance* Other,
+	OUT RValue& Result,
+	IN int ArgumentCount,
+	IN RValue** Arguments
+)
+{
+	if (load_on_start)
+	{
+		load_on_start = false;
+		LoadOrCreateConfigFile();
+		LoadSoundAssets();
+	}
+
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_SETUP_MAIN_SCREEN));
+	original(
+		Self,
+		Other,
+		Result,
+		ArgumentCount,
+		Arguments
+	);
+
+	return Result;
+}
+
+RValue& GmlScriptOnDrawGuiCallback(
+	IN CInstance* Self,
+	IN CInstance* Other,
+	OUT RValue& Result,
+	IN int ArgumentCount,
+	IN RValue** Arguments
+)
+{
+	for (auto& pair : sound_name_to_asset_index_map)
+	{
+		if (AudioIsPlaying(pair.second))
+		{
+			StopSoundEffect(pair.second);
+		}
+	}
+
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_ON_DRAW_GUI));
+	original(
+		Self,
+		Other,
+		Result,
+		ArgumentCount,
+		Arguments
+	);
+
+	return Result;
+}
+
+void CreateHookGmlScriptSetupMainScreen(AurieStatus& status)
+{
+	CScript* gml_script_setup_main_screen = nullptr;
+	status = g_ModuleInterface->GetNamedRoutinePointer(
+		GML_SCRIPT_SETUP_MAIN_SCREEN,
+		(PVOID*)&gml_script_setup_main_screen
+	);
+
+	if (!AurieSuccess(status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_SETUP_MAIN_SCREEN);
+	}
+
+	status = MmCreateHook(
+		g_ArSelfModule,
+		GML_SCRIPT_SETUP_MAIN_SCREEN,
+		gml_script_setup_main_screen->m_Functions->m_ScriptFunction,
+		GmlScriptSetupMainScreenCallback,
+		nullptr
+	);
+
+	if (!AurieSuccess(status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_SETUP_MAIN_SCREEN);
+	}
+}
+
+void CreateHookGmlScriptOnDrawGui(AurieStatus& status)
+{
+	CScript* gml_script_on_draw_gui = nullptr;
+	status = g_ModuleInterface->GetNamedRoutinePointer(
+		GML_SCRIPT_ON_DRAW_GUI,
+		(PVOID*)&gml_script_on_draw_gui
+	);
+
+	if (!AurieSuccess(status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_ON_DRAW_GUI);
+	}
+
+	status = MmCreateHook(
+		g_ArSelfModule,
+		GML_SCRIPT_ON_DRAW_GUI,
+		gml_script_on_draw_gui->m_Functions->m_ScriptFunction,
+		GmlScriptOnDrawGuiCallback,
+		nullptr
+	);
+
+	if (!AurieSuccess(status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_ON_DRAW_GUI);
+	}
+}
+
+EXPORTED AurieStatus ModuleInitialize(
+	IN AurieModule* Module,
+	IN const fs::path& ModulePath
+)
+{
+	UNREFERENCED_PARAMETER(ModulePath);
+
+	AurieStatus status = AURIE_SUCCESS;
+
+	status = ObGetInterface(
+		"YYTK_Main",
+		(AurieInterfaceBase*&)(g_ModuleInterface)
+	);
+
+	if (!AurieSuccess(status))
+		return AURIE_MODULE_DEPENDENCY_NOT_RESOLVED;
+
+	g_ModuleInterface->Print(CM_LIGHTAQUA, "[%s %s] - Plugin starting...", MOD_NAME, VERSION);
+
+	CreateHookGmlScriptSetupMainScreen(status);
+	if (!AurieSuccess(status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
+		return status;
+	}
+
+	CreateHookGmlScriptOnDrawGui(status);
+	if (!AurieSuccess(status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
+		return status;
+	}
+
+	g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Plugin started!", MOD_NAME, VERSION);
+	return AURIE_SUCCESS;
+}
