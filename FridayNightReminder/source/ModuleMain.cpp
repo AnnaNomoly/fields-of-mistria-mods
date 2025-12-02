240704ae0fde00c38b5d15500e4d4ecfe958c89d
AnnaNomoly <annanomoly@outlook.com>
2025-12-02 13:31:25 -0500

    Friday Night Reminder
    Version 1.1.0: Added configuration file for setting the time the notification is displayed. Updated to YYTK v4.

--- FridayNightReminder/source/ModuleMain.cpp
+++ FridayNightReminder/source/ModuleMain.cpp
@@ -1,26 +1,180 @@
-#include <map>
-#include <YYToolkit/Shared.hpp>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <unordered_set>
+#include <nlohmann/json.hpp>
+#include <YYToolkit/YYTK_Shared.hpp> // YYTK v4
 using namespace Aurie;
 using namespace YYTK;
+using json = nlohmann::json;
 
-static YYTKInterface* g_ModuleInterface = nullptr;
-
-static const char* const VERSION = "1.0.1";
+static const char* const MOD_NAME = "FridayNightReminder";
+static const char* const VERSION = "1.1.0";
+static const char* const NOTIFICATION_TIME_KEY = "notification_time_in_seconds";
+static const char* const GML_SCRIPT_CREATE_NOTIFICATION = "gml_Script_create_notification";
+static const char* const GML_SCRIPT_GET_MINUTES = "gml_Script_get_minutes";
+static const char* const GML_SCRIPT_CALENDAR_DAY = "gml_Script_day@Calendar@Calendar";
+static const char* const GML_SCRIPT_NEW_DAY = "gml_Script_new_day";
+static const char* const GML_SCRIPT_ON_ROOM_START = "gml_Script_on_room_start@WeatherManager@Weather";
+static const char* const GML_SCRIPT_SETUP_MAIN_SCREEN = "gml_Script_setup_main_screen@TitleMenu@TitleMenu";
+static const std::string VISIT_THE_INN_NOTIFICATION_KEY = "Mods/Notifications/Friday Night Reminder/visit_the_inn";
 static const int EIGHT_PM_IN_SECONDS = 72000;
 
+static YYTKInterface* g_ModuleInterface = nullptr;
 static bool notification_sent = false;
+static bool load_on_start = true;
 static bool is_friday = false;
+static int notification_time = EIGHT_PM_IN_SECONDS;
 
-void DisplayFridayNightNotification(CInstance* Self, CInstance* Other)
+void PrintError(std::exception_ptr eptr)
+{
+	try {
+		if (eptr) {
+			std::rethrow_exception(eptr);
+		}
+	}
+	catch (const std::exception& e) {
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Error: %s", MOD_NAME, VERSION, e.what());
+	}
+}
+
+json CreateConfigJson(bool use_defaults)
+{
+	json config_json = {
+		{ NOTIFICATION_TIME_KEY, use_defaults ? EIGHT_PM_IN_SECONDS : notification_time }
+	};
+	return config_json;
+}
+
+void LogDefaultConfigValues()
+{
+	notification_time = EIGHT_PM_IN_SECONDS;
+
+	g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Using DEFAULT \"%s\" value: %d!", MOD_NAME, VERSION, NOTIFICATION_TIME_KEY, EIGHT_PM_IN_SECONDS);
+}
+
+void CreateOrLoadConfigFile()
+{
+	// Load config file.
+	std::exception_ptr eptr;
+	try
+	{
+		// Try to find the mod_data directory.
+		std::string current_dir = std::filesystem::current_path().string();
+		std::string mod_data_folder = current_dir + "\\mod_data";
+		if (!std::filesystem::exists(mod_data_folder))
+		{
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"mod_data\" directory was not found. Creating directory: %s", MOD_NAME, VERSION, mod_data_folder.c_str());
+			std::filesystem::create_directory(mod_data_folder);
+		}
+
+		// Try to find the mod_data/FridayNightReminder directory.
+		std::string friday_night_reminder_folder = mod_data_folder + "\\FridayNightReminder";
+		if (!std::filesystem::exists(friday_night_reminder_folder))
+		{
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"FridayNightReminder\" directory was not found. Creating directory: %s", MOD_NAME, VERSION, friday_night_reminder_folder.c_str());
+			std::filesystem::create_directory(friday_night_reminder_folder);
+		}
+
+		// Try to find the mod_data/FridayNightReminder/FridayNightReminder.json config file.
+		bool update_config_file = false;
+		std::string config_file = friday_night_reminder_folder + "\\" + "FridayNightReminder.json";
+		std::ifstream in_stream(config_file);
+		if (in_stream.good())
+		{
+			try
+			{
+				json json_object = json::parse(in_stream);
+
+				// Check if the json_object is empty.
+				if (json_object.empty())
+				{
+					g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - No values found in mod configuration file: %s!", MOD_NAME, VERSION, config_file.c_str());
+					g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Add your desired values to the configuration file, otherwise defaults will be used.", MOD_NAME, VERSION);
+					LogDefaultConfigValues();
+				}
+				else
+				{
+					// Try loading the notification_time_in_seconds value.
+					if (json_object.contains(NOTIFICATION_TIME_KEY))
+					{
+						notification_time = json_object[NOTIFICATION_TIME_KEY];
+						if (notification_time < 21600 || notification_time > 86400)
+						{
+							g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Invalid \"%s\" value (%d) in mod configuration file: %s", MOD_NAME, VERSION, NOTIFICATION_TIME_KEY, notification_time, config_file.c_str());
+							g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Make sure the value is a valid integer between 21600 and 86400 (inclusive)!", MOD_NAME, VERSION);
+							g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using DEFAULT \"%s\" value: %d!", MOD_NAME, VERSION, NOTIFICATION_TIME_KEY, EIGHT_PM_IN_SECONDS);
+							notification_time = EIGHT_PM_IN_SECONDS;
+						}
+						else
+						{
+							g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using CUSTOM \"%s\" value: %d!", MOD_NAME, VERSION, NOTIFICATION_TIME_KEY, notification_time);
+						}
+					}
+					else
+					{
+						g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Missing \"%s\" value in mod configuration file: %s!", MOD_NAME, VERSION, NOTIFICATION_TIME_KEY, config_file.c_str());
+						g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Using DEFAULT \"%s\" value: %d!", MOD_NAME, VERSION, NOTIFICATION_TIME_KEY, EIGHT_PM_IN_SECONDS);
+					}
+				}
+
+				update_config_file = true;
+			}
+			catch (...)
+			{
+				eptr = std::current_exception();
+				PrintError(eptr);
+
+				g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to parse JSON from configuration file: %s", MOD_NAME, VERSION, config_file.c_str());
+				g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - Make sure the file is valid JSON!", MOD_NAME, VERSION);
+				LogDefaultConfigValues();
+			}
+
+			in_stream.close();
+		}
+		else
+		{
+			in_stream.close();
+
+			g_ModuleInterface->Print(CM_LIGHTYELLOW, "[%s %s] - The \"FridayNightReminder.json\" file was not found. Creating file: %s", MOD_NAME, VERSION, config_file.c_str());
+
+			json default_config_json = CreateConfigJson(true);
+			std::ofstream out_stream(config_file);
+			out_stream << std::setw(4) << default_config_json << std::endl;
+			out_stream.close();
+
+			LogDefaultConfigValues();
+		}
+
+		if (update_config_file)
+		{
+			json config_json = CreateConfigJson(false);
+			std::ofstream out_stream(config_file);
+			out_stream << std::setw(4) << config_json << std::endl;
+			out_stream.close();
+		}
+	}
+	catch (...)
+	{
+		eptr = std::current_exception();
+		PrintError(eptr);
+
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - An error occurred loading the mod configuration file.", MOD_NAME, VERSION);
+		LogDefaultConfigValues();
+	}
+}
+
+
+void DisplayFridayNightNotification(std::string notification_key, CInstance* Self, CInstance* Other)
 {
 	CScript* gml_script_create_notification = nullptr;
 	g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_create_notification",
+		GML_SCRIPT_CREATE_NOTIFICATION,
 		(PVOID*)&gml_script_create_notification
 	);
 
 	RValue result;
-	RValue notification = "misc_local/FridayNightReminder";
+	RValue notification = RValue(notification_key);
 	RValue* notification_ptr = &notification;
 	gml_script_create_notification->m_Functions->m_ScriptFunction(
 		Self,
@@ -42,19 +196,19 @@ RValue& GmlScriptGetMinutesCallback(
 	if (is_friday && !notification_sent)
 	{
 		bool send_notification = false;
-		if (ArgumentCount == 1 && Arguments[0]->m_Kind == VALUE_INT64 && Arguments[0]->m_i64 >= EIGHT_PM_IN_SECONDS)
+		if (ArgumentCount == 1 && Arguments[0]->m_Kind == VALUE_INT64 && Arguments[0]->m_i64 >= notification_time)
 			send_notification = true;
-		if (ArgumentCount == 1 && Arguments[0]->m_Kind == VALUE_REAL && Arguments[0]->m_Real >= EIGHT_PM_IN_SECONDS)
+		if (ArgumentCount == 1 && Arguments[0]->m_Kind == VALUE_REAL && Arguments[0]->m_Real >= notification_time)
 			send_notification = true;
 
 		if (send_notification)
 		{
-			DisplayFridayNightNotification(Self, Other);
+			DisplayFridayNightNotification(VISIT_THE_INN_NOTIFICATION_KEY, Self, Other);
 			notification_sent = true;
 		}
 	}
 
-	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_get_minutes"));
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_GET_MINUTES));
 	original(
 		Self,
 		Other,
@@ -74,7 +228,7 @@ RValue& GmlScriptCalendarDayCallback(
 	IN RValue** Arguments
 )
 {
-	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_day@Calendar@Calendar"));
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_CALENDAR_DAY));
 	original(
 		Self,
 		Other,
@@ -106,7 +260,7 @@ RValue& GmlScriptNewDayCallback(
 	notification_sent = false;
 	is_friday = false;
 
-	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_new_day"));
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_NEW_DAY));
 	original(
 		Self,
 		Other,
@@ -126,7 +280,7 @@ RValue& GmlScriptOnRoomStartCallback(
 	IN RValue** Arguments
 )
 {
-	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_on_room_start@WeatherManager@Weather"));
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_ON_ROOM_START));
 	original(
 		Self,
 		Other,
@@ -146,10 +300,18 @@ RValue& GmlScriptSetupMainScreenCallback(
 	IN RValue** Arguments
 )
 {
-	notification_sent = false;
-	is_friday = false;
+	if (load_on_start)
+	{
+		load_on_start = false;
+		CreateOrLoadConfigFile();
+	}
+	else
+	{
+		notification_sent = false;
+		is_friday = false;
+	}
 
-	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_setup_main_screen@TitleMenu@TitleMenu"));
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_SETUP_MAIN_SCREEN));
 	original(
 		Self,
 		Other,
@@ -165,18 +327,18 @@ void CreateHookGmlScriptGetMinutes(AurieStatus& status)
 {
 	CScript* gml_script_get_minutes = nullptr;
 	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_get_minutes",
+		GML_SCRIPT_GET_MINUTES,
 		(PVOID*)&gml_script_get_minutes
 	);
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to get script (gml_Script_get_minutes)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_GET_MINUTES);
 	}
 
 	status = MmCreateHook(
 		g_ArSelfModule,
-		"gml_Script_get_minutes",
+		GML_SCRIPT_GET_MINUTES,
 		gml_script_get_minutes->m_Functions->m_ScriptFunction,
 		GmlScriptGetMinutesCallback,
 		nullptr
@@ -184,7 +346,7 @@ void CreateHookGmlScriptGetMinutes(AurieStatus& status)
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to hook script (gml_Script_get_minutes)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_GET_MINUTES);
 	}
 }
 
@@ -192,18 +354,18 @@ void CreateHookGmlScriptCalendarDay(AurieStatus& status)
 {
 	CScript* gml_script_calendar_day = nullptr;
 	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_day@Calendar@Calendar",
+		GML_SCRIPT_CALENDAR_DAY,
 		(PVOID*)&gml_script_calendar_day
 	);
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to get script (gml_Script_day@Calendar@Calendar)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_CALENDAR_DAY);
 	}
 
 	status = MmCreateHook(
 		g_ArSelfModule,
-		"gml_Script_day@Calendar@Calendar",
+		GML_SCRIPT_CALENDAR_DAY,
 		gml_script_calendar_day->m_Functions->m_ScriptFunction,
 		GmlScriptCalendarDayCallback,
 		nullptr
@@ -211,7 +373,7 @@ void CreateHookGmlScriptCalendarDay(AurieStatus& status)
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to hook script (gml_Script_day@Calendar@Calendar)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_CALENDAR_DAY);
 	}
 }
 
@@ -219,18 +381,18 @@ void CreateHookGmlScriptNewDay(AurieStatus& status)
 {
 	CScript* gml_script_new_day = nullptr;
 	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_new_day",
+		GML_SCRIPT_NEW_DAY,
 		(PVOID*)&gml_script_new_day
 	);
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to get script (gml_Script_new_day)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_NEW_DAY);
 	}
 
 	status = MmCreateHook(
 		g_ArSelfModule,
-		"gml_Script_new_day",
+		GML_SCRIPT_NEW_DAY,
 		gml_script_new_day->m_Functions->m_ScriptFunction,
 		GmlScriptNewDayCallback,
 		nullptr
@@ -239,27 +401,27 @@ void CreateHookGmlScriptNewDay(AurieStatus& status)
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to hook script (gml_Script_new_day)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_NEW_DAY);
 	}
 }
 
 void CreateHookGmlScriptOnRoomStart(AurieStatus& status)
 {
-	CScript* gml_script_new_day = nullptr;
+	CScript* gml_script_on_room_start = nullptr;
 	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_on_room_start@WeatherManager@Weather",
-		(PVOID*)&gml_script_new_day
+		GML_SCRIPT_ON_ROOM_START,
+		(PVOID*)&gml_script_on_room_start
 	);
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to get script (gml_Script_on_room_start@WeatherManager@Weather)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_ON_ROOM_START);
 	}
 
 	status = MmCreateHook(
 		g_ArSelfModule,
-		"gml_Script_on_room_start@WeatherManager@Weather",
-		gml_script_new_day->m_Functions->m_ScriptFunction,
+		GML_SCRIPT_ON_ROOM_START,
+		gml_script_on_room_start->m_Functions->m_ScriptFunction,
 		GmlScriptOnRoomStartCallback,
 		nullptr
 	);
@@ -267,7 +429,7 @@ void CreateHookGmlScriptOnRoomStart(AurieStatus& status)
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to hook script (gml_Script_on_room_start@WeatherManager@Weather)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_ON_ROOM_START);
 	}
 }
 
@@ -275,18 +437,18 @@ void CreateHookGmlScriptSetupMainScreen(AurieStatus& status)
 {
 	CScript* gml_script_setup_main_screen = nullptr;
 	status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_setup_main_screen@TitleMenu@TitleMenu",
+		GML_SCRIPT_SETUP_MAIN_SCREEN,
 		(PVOID*)&gml_script_setup_main_screen
 	);
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to get script (gml_Script_setup_main_screen@TitleMenu@TitleMenu)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_SETUP_MAIN_SCREEN);
 	}
 
 	status = MmCreateHook(
 		g_ArSelfModule,
-		"gml_Script_setup_main_screen@TitleMenu@TitleMenu",
+		GML_SCRIPT_SETUP_MAIN_SCREEN,
 		gml_script_setup_main_screen->m_Functions->m_ScriptFunction,
 		GmlScriptSetupMainScreenCallback,
 		nullptr
@@ -295,7 +457,7 @@ void CreateHookGmlScriptSetupMainScreen(AurieStatus& status)
 
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Failed to hook script (gml_Script_setup_main_screen@TitleMenu@TitleMenu)!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_SETUP_MAIN_SCREEN);
 	}
 }
 
@@ -312,43 +474,43 @@ EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path&
 	if (!AurieSuccess(status))
 		return AURIE_MODULE_DEPENDENCY_NOT_RESOLVED;
 
-	g_ModuleInterface->Print(CM_LIGHTAQUA, "[FridayNightReminder %s] - Plugin starting...", VERSION);
+	g_ModuleInterface->Print(CM_LIGHTAQUA, "[%s %s] - Plugin starting...", MOD_NAME, VERSION);
 	
 	CreateHookGmlScriptGetMinutes(status);
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Exiting due to failure on start!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
 		return status;
 	}
 
 	CreateHookGmlScriptCalendarDay(status);
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Exiting due to failure on start!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
 		return status;
 	}
 
 	CreateHookGmlScriptNewDay(status);
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Exiting due to failure on start!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
 		return status;
 	}
 
 	CreateHookGmlScriptOnRoomStart(status);
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Exiting due to failure on start!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
 		return status;
 	}
 
 	CreateHookGmlScriptSetupMainScreen(status);
 	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_LIGHTRED, "[FridayNightReminder %s] - Exiting due to failure on start!", VERSION);
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
 		return status;
 	}
 
-	g_ModuleInterface->Print(CM_LIGHTGREEN, "[FridayNightReminder %s] - Plugin started!", VERSION);
+	g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Plugin started!", MOD_NAME, VERSION);
 	return AURIE_SUCCESS;
 }
