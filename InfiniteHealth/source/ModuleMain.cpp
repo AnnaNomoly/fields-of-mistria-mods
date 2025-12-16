bc9c491a83445ed907c1df243a69edfb5b29017d
AnnaNomoly <annanomoly@outlook.com>
2025-12-15 22:31:03 -0500

    Infinite Health
    Version 1.0.2 released: Updated to YYTK v4. Added version (RC) file to DLL. Minor code refactoring to match how newer mods are written.

--- InfiniteHealth/source/ModuleMain.cpp
+++ InfiniteHealth/source/ModuleMain.cpp
@@ -1,17 +1,25 @@
-#include <YYToolkit/Shared.hpp>
+#include <YYToolkit/YYTK_Shared.hpp> // YYTK v4
 using namespace Aurie;
 using namespace YYTK;
 
+static const char* const MOD_NAME = "InfiniteHealth";
+static const char* const VERSION = "1.0.2";
+static const char* const GML_SCRIPT_MODIFY_HEALTH = "gml_Script_modify_health@Ari@Ari";
+
 static YYTKInterface* g_ModuleInterface = nullptr;
 
-RValue& HookModifyHealth( IN CInstance* Self, IN CInstance* Other, OUT RValue& Result, IN int ArgumentCount, IN RValue** Arguments) {
-	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, "gml_Script_modify_health@Ari@Ari"));
-	
-	if (Arguments[0]->m_Real < 0) {
-		g_ModuleInterface->Print(CM_LIGHTGREEN, "[InfiniteHealth] - Prevented health from being reduced.");
+RValue& GmlScriptModifyHealthCallback(
+	IN CInstance* Self,
+	IN CInstance* Other,
+	OUT RValue& Result,
+	IN int ArgumentCount,
+	IN RValue** Arguments
+)
+{
+	if (Arguments[0]->m_Real < 0)
 		Arguments[0]->m_Real = 0;
-	}
 
+	const PFUNC_YYGMLScript original = reinterpret_cast<PFUNC_YYGMLScript>(MmGetHookTrampoline(g_ArSelfModule, GML_SCRIPT_MODIFY_HEALTH));
 	original(
 		Self,
 		Other,
@@ -23,44 +31,55 @@ RValue& HookModifyHealth( IN CInstance* Self, IN CInstance* Other, OUT RValue& R
 	return Result;
 }
 
-EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path& ModulePath) {
-	UNREFERENCED_PARAMETER(ModulePath);
-
-	AurieStatus last_status = AURIE_SUCCESS;
-	
-	last_status = ObGetInterface(
-		"YYTK_Main", 
-		(AurieInterfaceBase*&)(g_ModuleInterface)
+void CreateHookGmlScriptModifyHealth(AurieStatus& status)
+{
+	CScript* gml_script_modify_health = nullptr;
+	status = g_ModuleInterface->GetNamedRoutinePointer(
+		GML_SCRIPT_MODIFY_HEALTH,
+		(PVOID*)&gml_script_modify_health
 	);
 
-	if (!AurieSuccess(last_status))
-		return AURIE_MODULE_DEPENDENCY_NOT_RESOLVED;
-
-	g_ModuleInterface->Print(CM_LIGHTGREEN, "[InfiniteHealth] - Plugin loaded!");
-	
-	CScript* script_modify_health = nullptr;
-	last_status = g_ModuleInterface->GetNamedRoutinePointer(
-		"gml_Script_modify_health@Ari@Ari",
-		(PVOID*)&script_modify_health
-	);
-
-	if (!AurieSuccess(last_status))
+	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_RED, "[InfiniteHealth] - Failed to find script: gml_Script_modify_health@Ari@Ari !");
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to get script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_MODIFY_HEALTH);
 	}
 
-	last_status = MmCreateHook(
+	status = MmCreateHook(
 		g_ArSelfModule,
-		"gml_Script_modify_health@Ari@Ari",
-		script_modify_health->m_Functions->m_ScriptFunction,
-		HookModifyHealth,
+		GML_SCRIPT_MODIFY_HEALTH,
+		gml_script_modify_health->m_Functions->m_ScriptFunction,
+		GmlScriptModifyHealthCallback,
 		nullptr
 	);
 
-	if (!AurieSuccess(last_status))
+	if (!AurieSuccess(status))
 	{
-		g_ModuleInterface->Print(CM_RED, "[InfiniteHealth] - Failed to hook script: gml_Script_modify_health@Ari@Ari !");
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Failed to hook script (%s)!", MOD_NAME, VERSION, GML_SCRIPT_MODIFY_HEALTH);
+	}
+}
+
+EXPORTED AurieStatus ModuleInitialize(IN AurieModule* Module, IN const fs::path& ModulePath) {
+	UNREFERENCED_PARAMETER(ModulePath);
+
+	AurieStatus status = AURIE_SUCCESS;
+
+	status = ObGetInterface(
+		"YYTK_Main",
+		(AurieInterfaceBase*&)(g_ModuleInterface)
+	);
+
+	if (!AurieSuccess(status))
+		return AURIE_MODULE_DEPENDENCY_NOT_RESOLVED;
+
+	g_ModuleInterface->Print(CM_LIGHTAQUA, "[%s %s] - Plugin starting...", MOD_NAME, VERSION);
+
+	CreateHookGmlScriptModifyHealth(status);
+	if (!AurieSuccess(status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTRED, "[%s %s] - Exiting due to failure on start!", MOD_NAME, VERSION);
+		return status;
 	}
 
+	g_ModuleInterface->Print(CM_LIGHTGREEN, "[%s %s] - Plugin started!", MOD_NAME, VERSION);
 	return AURIE_SUCCESS;
 }
